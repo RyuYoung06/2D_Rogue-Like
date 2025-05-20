9f93c41495a5cebb4aff2b95c24088f811412e20
RyuYoung06 <yunggyu0511@gmail.com>
2025-05-20 16:02:52 +0900

    [Feat] (기말) 로그라이크 게임만들기

---
+++ Assets/Scripts/ItemSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Game/Item", fileName = "NewItem")]
+
+public class ItemSO : MonoBehaviour
+{
+    [Header("Score Value")]
+    public int point = 10;
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float mvoeSpeed = 2f;
+
+    [SerializeField] Sprite spriteUp;
+    [SerializeField] Sprite spriteDown;
+    [SerializeField] Sprite spriteLeft;
+    [SerializeField] Sprite spriteRight;
+
+    Rigidbody2D rb;
+    SpriteRenderer sR;
+
+    Vector2 input;
+    Vector2 velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sR = GetComponent<SpriteRenderer>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+        {
+            if (input.x > 0)
+                sR.sprite = spriteRight;
+            else if (input.x < 0)
+                sR.sprite = spriteLeft;
+
+        }
+        else
+        {
+            if (input.y > 0)
+                sR.sprite = spriteUp;
+            else if (input.y < 0)
+                sR.sprite = spriteDown;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+}
+
